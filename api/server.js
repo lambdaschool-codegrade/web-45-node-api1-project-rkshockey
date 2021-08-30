52f588bad80a5b7a1e581c1e487442152464025c
rkshockey <rkshockey@gmail.com>
2021-08-30 16:19:28 -0400

    Added both get requests

--- api/server.js
+++ api/server.js
@@ -1,3 +1,52 @@
 // BUILD YOUR SERVER HERE
+const express = require("express")
+const User = require("./users/model")
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if (user){
+                res.status(200).json(user)
+            }else{
+                res.status(404).json("User not found")
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err.message)
+        })
+})
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => res.status(200).json(users))
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err.message)
+        })
+})
+
+server.post('/api/users', (req, res) => {
+    User.insert(req.body)
+        .then(user => res.status(200).json(user))
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err.message)
+        })
+})
+
+server.put('/api/users/:id', (req, res) => {
+    res.json('Update user')
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    res.json('Delete user')
+})
+
+server.use('*', (req, res) => {
+    res.json('Wrong endpoint')
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const server = require('./api/server');
 
 const port = 5000;
 
+server.listen(port, () => {
+    console.log("Server is listening on port 5000")
+})
+
 // START YOUR SERVER HERE
